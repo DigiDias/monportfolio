6ccbb191a5409d39b2aaaff0bd99e41b938918e1
DigiDias <sammy.gouljiar@gmail.com>
2025-03-19 13:26:49 +0400

    finalisation de la structure de la page Portfolio et mise en place de la page services

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 
 const Portfolio = () => {
     return (
@@ -14,60 +15,71 @@ const Portfolio = () => {
 
         <div className="Contener-Card row">
 
-        <div className="card col-md-4 col-sm-12 mb-3" style={{width: "18rem"}}>
-  <img src="./img/fresh-food.jpg" className="card-img-top" alt="photoFresFood"></img>
+        <div className="card cogit l-md-4 col-sm-12 mb-3 px-0" style={{width: "18rem"}}>
+  <img src="./img/fresh-food.jpg" className="card-img-top" alt="photo FresFood"></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <p className="card-text">Site de vente de produits frais en lignes</p>
+    <a href="#" className="btn btn-primary">Voir le site</a>
+    
   </div>
+  <p className="sousTitre">Site réalisé avec PHP et MYSQL</p>
 </div>
 
 
-<div className="card" style={{width: "18rem"}}>
-  <img src="./img/restaurant-japonais.jpg" className="card-img-top" alt="PhotorestaurantJaponais"></img>
+<div className="card cogit l-md-4 col-sm-12 mb-3 px-0" style={{width: "18rem"}}>
+  <img src="./img/restaurant-japonais.jpg" className="card-img-top" alt="photo restaurant japonais"></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Voir le site</a>
+    
   </div>
+  <p className="sousTitre">Site réalisé avec PHP et MYSQL</p>
 </div>
 
-<div className="card" style={{width: "18rem"}}>
-  <img src="./img/espace-bien-etre.jpg" className="card-img-top" alt="PhotoespaceBienEtre"></img>
+<div className="card cogit l-md-4 col-sm-12 mb-3 px-0" style={{width: "18rem"}}>
+  <img src="./img/espace-bien-etre.jpg" className="card-img-top" alt="photo Espace Bien être"></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Voir le site</a>
+    
   </div>
+  <p className="sousTitre">Site réalisé avec PHP et MYSQL</p>
 </div>
 
-
-<div className="card" style={{width: "18rem"}}>
-  <img src="./img/seo.jpg" className="card-img-top" alt="photoSeo"></img>
+<div className="card cogit l-md-4 col-sm-12 mb-3 px-0" style={{width: "18rem"}}>
+  <img src="./img/seo.jpg" className="card-img-top" alt="photo SEO"></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Voir le site</a>
+    
   </div>
+  <p className="sousTitre">Site réalisé avec PHP et MYSQL</p>
 </div>
 
-<div className="card" style={{width: "18rem"}}>
-  <img src="./img/coder.jpg" className="card-img-top" alt="photoCoder"></img>
+<div className="card cogit l-md-4 col-sm-12 mb-3 px-0" style={{width: "18rem"}}>
+  <img src="./img/coder.jpg" className="card-img-top" alt="photo Coder"></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Voir le site</a>
+    
   </div>
+  <p className="sousTitre">Site réalisé avec PHP et MYSQL</p>
 </div>
 
-<div className="card" style={{width: "18rem"}}>
-  <img src="./img/screens.jpg" className="card-img-top" alt="photoSceens"></img>
+<div className="card cogit l-md-4 col-sm-12 mb-3 px-0" style={{width: "18rem"}}>
+  <img src="./img/screens.jpg" className="card-img-top" alt="photo Screens"></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Voir le site</a>
+    
   </div>
+  <p className="sousTitre">Site réalisé avec PHP et MYSQL</p>
 </div>
 
 
--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,14 +1,56 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 
 
 const Services = () => {
     return (
         <main style={{ paddingTop: "100px" }}>
         <div>
-            <h1>Ma page Service</h1>
+        <div className="center">
+            <h2 className="title-h2">Mon offre de services</h2>
+            <p>Voici les prestations sur lequelles je peux intervenir</p>
+            
+       <hr></hr>
+        </div>
+        <div className="Contener-Card row">
+
+        <div className="card" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">UX Design</h5>
+    <p className="card-text">l'UX Design est une discipline qui consite à concevoir des produits
+        (sitesweb, applications mobiles, logiciels, objects connéctés, etc) en plaçant l'utilisateur
+    </p>
+
+  </div>
+</div>
+
+
+<div className="card" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">Dévéloppement web</h5>
+    <p className="card-text">Le développement de sites web consites à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.)
+        et des frameworks (Boorstrap, react, Angular, ect).
+    </p>
+
+  </div>
+</div>
+
+<div className="card" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">Référencement</h5>
+    <p className="card-text">Le référencement naturel (SEO) est une technique qui consite à optimiser un site web pour le faire remonter dans les résultats
+        des moteurs de recherche (Google, Bing, Yahoo, ect.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur le site.
+    </p>
+
+  </div>
+</div>
+
+
+        </div>
         </div>
         </main>
+
     );
 }
 
