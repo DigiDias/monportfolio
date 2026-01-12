990e6505cf0261b5291cc8987fba846d85c09872
DigiDias <sammy.gouljiar@gmail.com>
2026-01-12 16:28:04 +0400

    rajout cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import mailRoute from "./routes/mailRoute.js";
 
 dotenv.config();
 
+app.use(cors());
 const app = express();
 app.use(express.json());
 app.use("/api/mail", mailRoute);
